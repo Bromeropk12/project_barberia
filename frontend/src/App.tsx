1effd7e6e3cfa11b3b676ccd3408269e2e4e95e0
Bromeropk12 <bromero12@uan.edu.co>
2025-11-20 12:34:11 -0500

    archivos y configuracion para subir a vercel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { User, Servicio, Barbero, Horario, Reserva, RegisterData } from './components/types';
